79ae49b1a8c243beece34771ccfa306c4fcdd545
TheNilesh <akhadenilesh93@gmail.com>
2015-04-15 18:20:33 +0530

    batch file

--- Bully/src/runProject.bat
+++ Bully/src/runProject.bat
@@ -1,7 +1,7 @@
 @echo off
 javac Bully.java
-java Bully procList.txt 1
-java Bully procList.txt 2
-java Bully procList.txt 3
-java Bully procList.txt 4
+start java Bully procList.txt 1
+start java Bully procList.txt 2
+start java Bully procList.txt 3
+start java Bully procList.txt 4
 java Bully procList.txt 5
